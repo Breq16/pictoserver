4958509dc6faffc564b45a3c4b789a6892891dd5
Brooke Chalmers <breq@breq.dev>
2021-12-15 17:55:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const channels = {};
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data, isBinary) {
+    const payload = JSON.parse(data);
+
+    if (payload.type === "join") {
+      channels[payload.channel] = channels[payload.channel] || [];
+      channels[payload.channel].push(ws);
+      ws.send(
+        JSON.stringify({
+          type: "joined",
+          room: payload.channel,
+        })
+      );
+    } else if (payload.type === "message") {
+      channels[payload.channel].forEach(function (client) {
+        client.send(
+          JSON.stringify({
+            type: "message",
+            room: payload.channel,
+            message: payload.message,
+          })
+        );
+      });
+    }
+  });
+});
